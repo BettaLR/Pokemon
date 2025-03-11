a64b12273e2e19315c772ec1f6c6321f0f850edc
Manuel Cano <alexcanolara818@gmail.com>
2025-03-11 10:11:52 -0600

    UI Finished

--- pokemon/lib/main.dart
+++ pokemon/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon/button.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,8 +18,101 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void moveUp() {}
+  void moveDown() {}
+  void moveLeft() {}
+  void moveRight() {}
+  void pressedA() {}
+  void pressedB() {}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          AspectRatio(aspectRatio: 1, child: Container(color: Colors.black)),
+          Expanded(
+            child: Container(
+              color: Colors.grey[900],
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'G A M E B O Y',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Text(
+                          '   ❤️   ',
+                          style: TextStyle(color: Colors.red, fontSize: 20),
+                        ),
+                        Text(
+                          'F L U T T E R',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Container(height: 50, width: 50),
+                                MyButton(text: '←', function: moveLeft),
+                                Container(height: 50, width: 50),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                MyButton(text: '↑', function: moveUp),
+                                Container(height: 50, width: 50),
+                                MyButton(text: '↓', function: moveDown),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Container(height: 50, width: 50),
+                                MyButton(text: '→', function: moveRight),
+                                Container(height: 50, width: 50),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Container(height: 50, width: 50),
+                                MyButton(text: 'b', function: pressedB),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                MyButton(text: 'a', function: pressedA),
+                                Container(height: 50, width: 50),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'C R E A T E D B Y B E T T A',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
